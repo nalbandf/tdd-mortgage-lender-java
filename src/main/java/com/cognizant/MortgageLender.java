7bd90f7898a476d885d666086b0e6381ad1eb909
Joshi <256326@cognizant.com>
2021-05-01 15:20:25 -0400

    update bankrupt test cases

--- src/main/java/com/cognizant/MortgageLender.java
+++ src/main/java/com/cognizant/MortgageLender.java
@@ -2,34 +2,87 @@ package com.cognizant;
 
 public class MortgageLender {
 
-     int availableFundsAmount;
+    int availableFundsAmount;
 
-    public int getAvailableFunds(){
+    public int getAvailableFunds() {
         return availableFundsAmount;
     }
 
 
     public void depositFunds(int amount) {
-        availableFundsAmount+=amount;
+        availableFundsAmount += amount;
     }
 
     public Loan processLoan(LoanApplicant la, int requestedAmount) {
-        Loan loan = new Loan();
-        int savingsWorth = (requestedAmount*100)/la.getSavings();
-        System.out.println("savingsWorth is "+savingsWorth);
-        if(la.getDti()<36 && la.getCredit_score()>620 && savingsWorth>=25){
-            loan.setLoan_amount(requestedAmount);
-            loan.setLoanApplicant(la);
-            loan.setQualification("qualified");
-            loan.setStatus("qualified");
+        Loan loanObject;
+
+        try {
+
+            if(availableFundsAmount<requestedAmount) {
+                    loanObject = new Loan();
+                    loanObject.setStatus("On Hold");
+                    loanObject.setLoan_amount(requestedAmount);
+
+            }
+            else {
+                loanObject = checkLoanQualification(la, requestedAmount);
+                if(loanObject.getQualification().equalsIgnoreCase("not qualified")){
+                    loanObject.setStatus("DO NOT PROCEED");
+
+                }
+                else {
+                    loanObject.setStatus("Approved");
+                }
+            }
+            return loanObject;
+
+
+        }catch(NullPointerException ne){
+           throw new NullPointerException("null pointer exception");
         }
-        return loan;
+
 
     }
 
+    public Loan checkLoanQualification(LoanApplicant la, int requestedAmount) {
+        Loan loan;
+        if (la != null) {
 
+            loan = new Loan();
 
+        int savingsWorth = (la.getSavings() * 100) / requestedAmount;
 
+            if (la.getDti() < 36 && la.getCredit_score() > 620 && savingsWorth >= 25) {
+                loan.setLoan_amount(requestedAmount);
+                loan.setLoanApplicant(la);
+                loan.setQualification("qualified");
+                loan.setStatus("qualified");
+            } else if (la.getDti() < 36 && la.getCredit_score() > 620 && savingsWorth < 25) {
+
+                loan.setLoan_amount(la.getSavings() * 4);
+                loan.setLoanApplicant(la);
+                loan.setQualification("partially qualified");
+                loan.setStatus("qualified");
+
+            } else if (la.getDti() < 36 && la.getCredit_score() < 620 && savingsWorth >= 25) {
+
+                loan.setLoan_amount(0);
+                loan.setLoanApplicant(la);
+                loan.setQualification("not qualified");
+                loan.setStatus("denied");
+
+            } else if (la.getDti() > 36 && la.getCredit_score() > 620 && savingsWorth >= 25) {
+                loan.setLoan_amount(0);
+                loan.setLoanApplicant(la);
+                loan.setQualification("not qualified");
+                loan.setStatus("denied");
+            }
+
+            return loan;
+        }
+        else
+            throw new NullPointerException(" Loan Object is Null");
+    }
 
 
 
--- src/test/java/MortgageLenderTest.java
+++ src/test/java/MortgageLenderTest.java
@@ -14,49 +14,132 @@ public class MortgageLenderTest {
     MortgageLender ml;
 
     @BeforeEach
-    public void setUp(){
-       ml = new MortgageLender();
+    public void setUp() {
+        ml = new MortgageLender();
     }
 
     @Test
-    public void testCheckZeroAvailableFunds(){
+    public void testCheckZeroAvailableFunds() {
         int availableFunds = ml.getAvailableFunds();
-        assertEquals(0,availableFunds);
+        assertEquals(0, availableFunds);
     }
 
     @Test
-    public void testCheckAvailableFundsIsNotZero(){
-         ml.depositFunds(100000);
-        assertNotEquals(0,ml.getAvailableFunds());
-    }
-
-    @Test
-    public void testGetAvailableFunds(){
+    public void testCheckAvailableFundsIsNotZero() {
         ml.depositFunds(100000);
-        assertEquals(100000,ml.getAvailableFunds());
+        assertNotEquals(0, ml.getAvailableFunds());
     }
 
     @Test
-    public void testAddAvailableFunds(){
+    public void testGetAvailableFunds() {
+        ml.depositFunds(100000);
+        assertEquals(100000, ml.getAvailableFunds());
+    }
+
+    @Test
+    public void testAddAvailableFunds() {
         ml.depositFunds(100000);
         int current_amount = ml.getAvailableFunds();
-        assertTrue(current_amount>0);
+        assertTrue(current_amount > 0);
         int deposit_amount = 200000;
-        assertNotEquals(deposit_amount,ml.getAvailableFunds());
+        assertNotEquals(deposit_amount, ml.getAvailableFunds());
         ml.depositFunds(deposit_amount);
-        int total= current_amount+deposit_amount;
-        assertTrue(total==ml.getAvailableFunds());
+        int total = current_amount + deposit_amount;
+        assertTrue(total == ml.getAvailableFunds());
 
     }
 
     @Test
-    public void testQualifyFullLoanAmountSuccess(){
-        LoanApplicant la = new LoanApplicant(21,700,100000);
-        int requestedAmount = 25000;
-        Loan loan = ml.processLoan(la,requestedAmount);
-        assertEquals("qualified",loan.getQualification());
-        assertEquals(25000, loan.getLoan_amount());
-        assertEquals("qualified",loan.getStatus());
+    public void testQualifyFullLoanAmountSuccess() {
+        LoanApplicant la = new LoanApplicant(21, 700, 100000);
+        int requestedAmount = 250000;
+        Loan loan = ml.processLoan(la, requestedAmount);
+        assertEquals("qualified", loan.getQualification());
+        assertEquals(250000, loan.getLoan_amount());
+        assertEquals("qualified", loan.getStatus());
     }
+
+    @Test
+    public void testNotQualifiedForGreaterDTI() {
+        LoanApplicant la = new LoanApplicant(37, 700, 100000);
+        int requestedAmount = 250000;
+        Loan loan = ml.processLoan(la, requestedAmount);
+        assertEquals("not qualified", loan.getQualification());
+        assertEquals(0, loan.getLoan_amount());
+        assertEquals("denied", loan.getStatus());
+    }
+
+    @Test
+    public void testNotQualifiedWithLessCreditScore() {
+        LoanApplicant la = new LoanApplicant(30, 600, 100000);
+        int requestedAmount = 250000;
+        Loan loan = ml.processLoan(la, requestedAmount);
+        assertEquals("not qualified", loan.getQualification());
+        assertEquals(0, loan.getLoan_amount());
+        assertEquals("denied", loan.getStatus());
+    }
+
+    @Test
+    public void testPartiallyQualified() {
+        LoanApplicant la = new LoanApplicant(30, 700, 50000);
+        int requestedAmount = 250000;
+        Loan loan = ml.processLoan(la, requestedAmount);
+        assertEquals("partially qualified", loan.getQualification());
+        assertEquals(200000, loan.getLoan_amount());
+        assertEquals("qualified", loan.getStatus());
+    }
+
+    @Test
+    public void testNotNullLoanObject() {
+        LoanApplicant la = new LoanApplicant(30, 700, 50000);
+        int requestedAmount = 250000;
+        Loan loan = ml.processLoan(la, requestedAmount);
+        assertNotNull(loan);
+    }
+
+    @Test
+    public void testNullLoanObject() {
+        LoanApplicant la = null;
+        int requestedAmount = 250000;
+        // Loan loan = ml.processLoan(la,requestedAmount);
+        assertThrows(NullPointerException.class, () -> {
+            ml.processLoan(la, requestedAmount);
+        });
+
+    }
+
+    @Test
+    public void testCheckLoanStatusOnHold(){
+        ml.depositFunds(100000);
+        LoanApplicant la = new LoanApplicant(30, 700, 50000);
+        int requestedAmount = 125000;
+        Loan loan = ml.processLoan(la, requestedAmount);
+        assertEquals("On Hold", loan.getStatus());
+
+
+    }
+
+    @Test
+    public void testCheckLoanStatusApprovedWithQualified(){
+        ml.depositFunds(200000);
+        LoanApplicant la = new LoanApplicant(30, 700, 100000);
+        int requestedAmount = 125000;
+        Loan loan = ml.processLoan(la, requestedAmount);
+        assertEquals("Approved", loan.getStatus());
+
+
+    }
+    @Test
+    public void testCheckLoanStatusApprovedWithoutQualified(){
+        ml.depositFunds(200000);
+        LoanApplicant la = new LoanApplicant(37, 700, 100000);
+        int requestedAmount = 125000;
+        Loan loan = ml.processLoan(la, requestedAmount);
+        assertEquals("DO NOT PROCEED", loan.getStatus());
+
+
+    }
+
+
 
 }
